efc4d11674336014fb67170dfb8a818d07dfd0a3
平奕峰 <18989893657@189.cn>
2015-11-03 17:28:54 +0800

    v 1.0

---
+++ format.js
@@ -0,0 +1,26 @@
+/**
+ * String
+ * @authors pingyf(koakumaping@163.com)
+ * @date    2015-11-03 14:00:21
+ * @version $Id$
+ */
+
+(function(window){
+	var format = function(){
+		if (arguments.length == 0){
+			return '';
+		} else {
+			if (arguments.length == 1){
+				return arguments[0];
+			} else {
+				var toReplace = arguments[0]
+				for(var index = 1; index < arguments.length; index++){
+					toReplace = toReplace.replace('{'+ (index-1) +'}', arguments[index]);
+				}
+				return toReplace;
+			}
+		}
+	};
+
+	window.format = window.format || format;
+})(window);
