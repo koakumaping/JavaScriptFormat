6ae345374778d6c15a1daaaaa3f896b1929dd7aa
平奕峰 <18989893657@189.cn>
2015-11-23 09:36:24 +0800

    update to v2.0 final

--- format.js
+++ format.js
@@ -1,15 +1,23 @@
 /**
- * String
+ * format
  * @authors pingyf(koakumaping@163.com)
  * @date    2015-11-03 14:00:21
  * @version $Id$
  */
 
 (function(window){
+	Function.prototype.uncurrying = function(){
+		var self = this;
+		return function(){
+			return Function.prototype.call.apply(self, arguments);
+		};
+	};
+	
 	var format = function(){
 		if (arguments.length == 0){
 			return '';
 		} else {
+
 			if (arguments.length == 1){
 				return arguments[0];
 			} else {
@@ -19,8 +27,11 @@
 				}
 				return toReplace;
 			}
+
 		}
 	};
+
+	format = format.uncurrying();
 
 	window.format = window.format || format;
 })(window);
